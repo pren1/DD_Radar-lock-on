5b8f605c6fcef8e351082e19340e66053f69a11e
ScilientHill <pren1@ucsc.edu>
2019-12-20 21:35:33 -0800

    With Man name

--- Dataset_searcher.py
+++ Dataset_searcher.py
@@ -2,6 +2,7 @@
 import pdb
 import sqlite3
 import time
+import requests
 
 class Dataset_searcher(object):
 	def __init__(self, data_set_name):
@@ -12,15 +13,23 @@ class Dataset_searcher(object):
 		self.c = self.conn.cursor()
 		print("Opened database successfully")
 
-	def select_simultaneous_interpretation_man(self):
+	def select_simultaneous_interpretation_man(self, man_threshold):
+		'man_threshold: Only record the UID whose interpretation is above this threshold'
 		cursor = self.c.execute("SELECT UID, message, count(UID) "
 		                        "from DD_DANMAKU_TABLE WHERE substr(message,1,1) = '【' AND substr(message,-1,1) = '】'"
 		                        "GROUP by UID ORDER BY count(UID) DESC;")
-		simultaneous_interpretation_man_list = [(row[0], row[-1]) for row in cursor if row[-1] > 100]
+		simultaneous_interpretation_man_list = [(self.show_me_your_id(row[0]), row[-1]) for row in cursor if row[-1] > man_threshold]
+		return simultaneous_interpretation_man_list
+
+	def show_me_your_id(self, UID):
+		url = f'https://api.bilibili.com/x/space/acc/info?mid={UID}'
+		res = requests.get(url)
+		return res.json()['data']['name']
 
 if __name__ == '__main__':
 	ds = Dataset_searcher("test.db")
 	ds.connect_dataset()
 	start_time = time.time()
-	ds.select_simultaneous_interpretation_man()
+	simultaneous_interpretation_man_list = ds.select_simultaneous_interpretation_man(man_threshold=100)
 	print("simultaneous_interpretation_man selected within {}".format("--- %s seconds ---" % (time.time() - start_time)))
+	print(simultaneous_interpretation_man_list)
