811b91c605c78cd0df30ab4fdd61ebe55931029e
ScilientHill <pren1@ucsc.edu>
2019-12-21 22:23:57 -0800

    Build live chart for room

--- Dataset_searcher.py
+++ Dataset_searcher.py
@@ -11,33 +11,43 @@ import csv
 class Dataset_searcher(object):
 	def __init__(self, data_set_name):
 		self.data_set_name = data_set_name
-		reader = csv.reader(open('room_id_nick_name_chart.csv'))
-		self.room_id_df = {}
+		self.room_id_df = self.read_in_target_csv(csv_name='room_id_nick_name_chart.csv')
+		self.interpretation_man_df = self.read_in_target_csv(csv_name='man_id_nick_name_chart.csv')
+
+	def read_in_target_csv(self, csv_name):
+		reader = csv.reader(open(csv_name))
+		room_id_df = {}
 		for row in reader:
 			key = row[0].split('\t')[0]
 			if len(key) == 0:
 				# ignore head
-				pass
-			if key in self.room_id_df:
+				continue
+			if key in room_id_df:
 				# implement your duplicate row handling here
-				assert 1==0, "Error read in dict"
-			self.room_id_df[key] = row[0].split('\t')[1]
+				assert 1 == 0, "Error read in dict"
+			room_id_df[key] = row[0].split('\t')[1]
+		return room_id_df
 
 	def connect_dataset(self):
 		self.conn = sqlite3.connect(self.data_set_name)
 		self.c = self.conn.cursor()
 		print("Opened database successfully")
-		# Run this one time, please keep this line Commented unless you loss room_id_nick_name_chart.csv or you wanna update it
-		# self.build_fast_name_chart()
 
 	def build_fast_name_chart(self):
-		'First, get all the room_id'
+		'Build this for fast room nick name fetch'
 		cursor = self.c.execute("SELECT room_id from DD_DANMAKU_TABLE;")
 		room_id_set = set([row for row in cursor])
 		df = pd.DataFrame(columns=['room_nick_name'])
 		for single in tqdm(room_id_set):
 			df.loc[single] = self.show_me_your_room_id(single[0])
 		df.to_csv("room_id_nick_name_chart.csv", sep='\t', encoding='utf-8')
+
+	def build_simultaneous_interpretation_man_name_chart(self, pure_uid_man_list):
+		'Build this for fast simultaneous_interpretation_man nick name fetch'
+		df = pd.DataFrame(columns=['man_nick_name'])
+		for single in tqdm(pure_uid_man_list):
+			df.loc[single] = self.show_me_your_id(single, show_name=True)
+		df.to_csv("man_id_nick_name_chart.csv", sep='\t', encoding='utf-8')
 
 	def select_simultaneous_interpretation_man(self, man_threshold, show_name):
 		'man_threshold: Only record the UID whose interpretation is above this threshold'
@@ -65,6 +75,10 @@ class Dataset_searcher(object):
 	def prefetched_room_id(self, room_id):
 		'Get prefetched room id name, this method is faster than show_me_your_room_id'
 		return self.room_id_df[room_id]
+
+	def prefetched_man_id(self, man_id):
+		'Get prefetched man id name, this method is faster than show_your_id'
+		return self.interpretation_man_df[man_id]
 
 	def select_date_within(self, start_date, end_date):
 		'Check interpretations within an time interval, deprecated, and never used. Just for debug purpose'
@@ -105,7 +119,7 @@ class Dataset_searcher(object):
 		                        "GROUP by YMD_time, room_id ORDER BY YMD_time;", (input_UID,))
 		return [[row[0], self.prefetched_room_id(row[1]), row[2], row[4]] for row in cursor]
 
-	def Single_live_roow_interpretation_timeline(self, input_room_id, pure_uid_man_list):
+	def Single_live_room_interpretation_timeline(self, input_room_id, pure_uid_man_list):
 		'Show the timeline of interpretation within the target live room'
 		'Return format: (YMD_time, room_id, user_id, danmaku_count)'
 		man_list_length = len(pure_uid_man_list)
@@ -114,7 +128,7 @@ class Dataset_searcher(object):
 		cursor = self.c.execute("SELECT YMD_time, room_id, UID, message, count(message) "
 		                        "from DD_DANMAKU_TABLE WHERE substr(message,1,1) = '【' AND substr(message,-1,1) = '】' AND UID != '114514' AND room_id = ? AND UID in ({seq})"
 		                        "GROUP by YMD_time, UID ORDER BY YMD_time;".format(seq=','.join(['?']*man_list_length)), pure_uid_man_list)
-		return [(row[0], row[1], row[2], row[4]) for row in cursor]
+		return [(row[0], row[1], self.prefetched_man_id(row[2]), row[4]) for row in cursor]
 
 if __name__ == '__main__':
 	ds = Dataset_searcher("test.db")
@@ -122,9 +136,10 @@ if __name__ == '__main__':
 	all_interpretation = ds.show_target_interpretation_from_UID(input_UID='739848')
 	simultaneous_interpretation_man_list = ds.select_simultaneous_interpretation_man(man_threshold=100, show_name=False)
 	date_within = ds.select_date_within(start_date='2019-09-01', end_date='2019-09-30')
-	all_interpretation = ds.show_target_interpretation_from_UID(input_UID='739848')
 	all_danmaku = ds.show_all_danmaku_from_UID(input_UID='268065764')
 	single_uid_interpretation_timeline = ds.Single_UID_interpretation_timeline(input_UID='37718180')
 	single_uid_all_danmaku_timeline = ds.Single_UID_all_danmaku_timeline(input_UID='37718180')
 	pure_uid_man_list = [row[0] for row in simultaneous_interpretation_man_list]
-	single_live_roow_interpretation_timeline = ds.Single_live_roow_interpretation_timeline(input_room_id='11588230', pure_uid_man_list=pure_uid_man_list)
+	ds.build_simultaneous_interpretation_man_name_chart(pure_uid_man_list=pure_uid_man_list)
+	single_live_roow_interpretation_timeline = ds.Single_live_room_interpretation_timeline(input_room_id='11588230', pure_uid_man_list=pure_uid_man_list)
+	ds.build_fast_name_chart()
--- Naive_data_insight.py
+++ Naive_data_insight.py
@@ -25,6 +25,28 @@ class Naive_data_insight(object):
 		df = self.time_line_to_dataframe(interpretation_man_rank_list, columns=['昵称', '同传弹幕数'])
 		df.to_csv(csv_name, sep='\t', encoding='utf-8')
 
+	def visualize_single_room_timeline(self, room_id):
+		def visualize_dataframe(new_df, title):
+			plt.rcParams['font.sans-serif'] = ['SimHei']
+			new_df.loc[:, 'danmaku_count'] = new_df['danmaku_count'].astype(float)
+			new_df.YMD_time = pd.to_datetime(new_df['YMD_time'])
+			g = sns.FacetGrid(new_df, hue="user_id", height=8, aspect=2)
+			g.map(plt.scatter, "YMD_time", "danmaku_count")
+			g.fig.legend(loc='best', shadow=True)
+			g.map(plt.plot, "YMD_time", "danmaku_count")
+			plt.title(title)
+			plt.xlabel('时间轴')
+			plt.ylabel('弹幕数')
+			plt.gcf().autofmt_xdate()
+			plt.subplots_adjust(left=0.08, bottom=0.10, right=0.98, top=0.93, wspace=0.05, hspace=0.05)
+			plt.show()
+
+		Single_room_id_timeline = self.time_line_to_dataframe(
+			self.ds.Single_live_room_interpretation_timeline(input_room_id=room_id, pure_uid_man_list=self.pure_uid_man_list),
+			columns=['YMD_time', 'room_id', 'user_id', 'danmaku_count'])
+		room_id_nick_name = self.ds.prefetched_room_id(room_id)
+		visualize_dataframe(Single_room_id_timeline, title=f"{room_id_nick_name}: 直播间同传统计")
+
 	def visualize_single_uid_timeline(self, input_UID):
 		def visualize_dataframe(new_df, title):
 			plt.rcParams['font.sans-serif']=['SimHei']
@@ -64,4 +86,5 @@ if __name__ == '__main__':
 	NDI = Naive_data_insight()
 	# NDI.output_interpretation_man_rank_csv(csv_name="interpretation_man_rank.csv")
 	# pdb.set_trace()
+	NDI.visualize_single_room_timeline(room_id='11588230')
 	NDI.visualize_single_uid_timeline(input_UID='739848')
